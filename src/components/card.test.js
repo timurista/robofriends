0f07616891faa639d95dadc2d38b9d6bd469bdc8
Tim Urista <timothy.urista@gmail.com>
2018-08-11 20:03:27 -0700

    add basic test to card component

---
+++ src/components/card.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow, render } from 'enzyme'
+import Card from './card'
+
+
+describe('card component', () => {
+  const setup = ({props: {}}, () => {
+    const newProps = {...props}
+    return shallow(<Card {...newProps}/>)
+  })
+
+  it('card component exists', () => {
+    expect(shallow(<Card />)).toHaveLength(1)
+  })
+})
