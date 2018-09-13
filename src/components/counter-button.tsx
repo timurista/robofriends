08ae02104f5215ead2e4a21a4d1466419c922a3f
Tim Urista <timothy.urista@gmail.com>
2018-09-13 08:16:49 -0700

    adding prettier

--- src/components/counter-button.tsx
+++ src/components/counter-button.tsx
@@ -1,32 +1,36 @@
-import * as React from 'react'
+import * as React from "react";
 
 type Props = any;
 
 type State = {
   count: number;
-}
+};
 
 class Header extends React.Component<Props, State> {
   state = {
-    count: 0,
-  }
+    count: 0
+  };
 
   updateCount = () => {
-    this.setState(state => ({ count: state.count + 1}))
-  }
+    this.setState(state => ({ count: state.count + 1 }));
+  };
 
   shouldComponentUpdate(nextState) {
     if (this.state.count !== nextState.count) {
-      return true
+      return true;
     }
     return false;
   }
   render() {
     return (
-      <button data-test-id="counter-button" color={this.props.color} onClick={this.updateCount}>
+      <button
+        data-test-id="counter-button"
+        color={this.props.color}
+        onClick={this.updateCount}
+      >
         Count: {this.state.count}
       </button>
-    )
+    );
   }
 }
 
